c6d6254d9b128c95b60bcb4b4de93817314af201
Futa HIRAKOBA <korosuke613@users.noreply.github.com>
2018-05-17 16:52:05 +0900

    Update reference.c

--- src/sample/reference.c
+++ src/sample/reference.c
@@ -1,16 +1,15 @@
 // Copyright 2018 T.Miyaji, Futa HIRAKOBA
 
 // C言語流に、関数内で実変数の値が変わるコードを書け
-# include <stdio.h>
+#include <stdio.h>
 
 // ここに関数funcを作成する。
 
 
-int main()
-{
+int main(){
   int x = 0;
 
-  func(&x);
+  func;
 
   printf("%d\n", x); // xの値が1になっている
 }
